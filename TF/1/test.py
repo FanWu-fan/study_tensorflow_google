48f4705f2b4208812f50186f07e9395fd27b64a1
wufan <583221850@qq.com>
2019-01-18 20:07:19 +0800

    张量的使用

--- TF/1/test.py
+++ TF/1/test.py
@@ -1,5 +1,17 @@
 import tensorflow as tf 
-a = tf.constant([1.0, 2.0], name = "a")
-b = tf.constant([2.0, 3.0], name = "b")
-result = a + b
-print(result)
+
+def A():
+    a = tf.constant([1.0, 2.0], name = "a")
+    b = tf.constant([2.0, 3.0], name = "b")
+    #result = a + b
+    result = tf.add(a, b, name = "add")
+    print(result)
+
+def B():
+    pass
+
+
+
+
+if __name__ == "__main__":
+    A()
