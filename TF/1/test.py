4f3d6718afce2f68e477504e4a05818ffa783708
wufan <583221850@qq.com>
2019-01-20 15:48:40 +0800

    3.2节 TF实现神经网络

--- TF/1/test.py
+++ TF/1/test.py
@@ -5,7 +5,28 @@ def A():
     b = tf.constant([2.0, 3.0], name = "b")
     #result = a + b
     result = tf.add(a, b, name = "add")
-    print(result)
+    # print(result)
+    #print(tf.Session().run(result))
+    # tf.Session().close()
+
+    # with sess.as_default():
+    #     print(result.eval())
+
+    # print(sess.run(result))
+    # print(result.eval(session = sess))
+    # tf.Session().close()
+
+    # sess = tf.InteractiveSession()
+    # print(result.eval())
+    # sess.close()
+    
+    config = tf.ConfigProto(allow_soft_placement = False,
+                            log_device_placement = True)
+    sess1 = tf.InteractiveSession(config = config)
+    sess2 = tf.Session(config = config)
+    print(result.eval())
+
+
 
 def B():
     pass
